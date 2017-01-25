12755451fe7fc3002849ad18d3f8539d817da8d5
Leonard <leonardbinet@gmail.com>
2017-01-25 19:40:22 +0100

    Task for populating postgres

--- src/task_01_w_extract_schedule.py
+++ src/task_01_w_extract_schedule.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     from src.utils_misc import set_logging_conf
     set_logging_conf(log_name="task02_w_dl_gtfs.log")
 
-from src.mod_02_find_schedule import download_gtfs_files, write_flat_departures_times_df
+from src.mod_01_extract_schedule import download_gtfs_files, write_flat_departures_times_df, save_all_schedule_tables_rdb
 
 logger = logging.getLogger(__name__)
 
@@ -23,3 +23,6 @@ download_gtfs_files()
 
 logging.info("Create flat csv.")
 write_flat_departures_times_df()
+
+logging.info("Inserting data in postgres")
+save_all_schedule_tables_rdb()
