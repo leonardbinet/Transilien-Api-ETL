b1a1e169024e2a6485583c1f1ae2c152d3d0e911
Leonard <leonardbinet@gmail.com>
2017-01-30 15:45:38 +0100

    Cleanup, add LICENSE, add pipy setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='api_transilien_manager',
+    packages=['api_transilien_manager'],
+    version='0.0.1',
+    description='Task manager to exploit data from transilien\'s api',
+    author='Leonard Binet',
+    author_email='leonardbinet@gmail.com',
+    license='MIT',
+    url='https://github.com/leonardbinet/Transilien-Api',
+    classifiers=[],
+)
