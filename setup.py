a84e0c995742ed41353ee40b59d3c1718c4cabd5
Leonard <leonardbinet@gmail.com>
2017-01-30 16:31:48 +0100

    Tasks updated with new package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 setup(
     name='api_transilien_manager',
     packages=['api_transilien_manager'],
-    version='0.0.1',
+    version='0.0.2',
     description='Task manager to exploit data from transilien\'s api',
     author='Leonard Binet',
     author_email='leonardbinet@gmail.com',
