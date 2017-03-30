ddd37e0200cbdbe31ec9a162c19183d444fafb13
Leonard <leonardbinet@gmail.com>
2017-03-30 13:23:31 +0200

    Cleaning: tasks reordering

--- tasks/weekly_gtfs_update.py
+++ tasks/weekly_gtfs_update.py
@@ -7,12 +7,18 @@ if __name__ == '__main__':
     from api_etl.utils_misc import set_logging_conf
     set_logging_conf(log_name="download_gtfs_files")
 
-    from api_etl.extract_schedule import download_gtfs_files
+    from api_etl.extract_schedule import ScheduleExtractorRDB
 
     logger = logging.getLogger(__name__)
 
     # This operation is done every week
-    logger.info("Task: weekly update of gtfs files")
+    logger.info("Task: weekly update of gtfs files in two steps:"
+                + "download, then save in database.")
+
+    schex = ScheduleExtractorRDB()
 
     logger.info("Download files.")
-    download_gtfs_files()
+    schex.download_gtfs_files()
+
+    logger.info("Save in database.")
+    schex.save_in_rdb()
