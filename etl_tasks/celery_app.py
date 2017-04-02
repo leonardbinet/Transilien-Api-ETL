9cb16bd08f2532bf8e82ca0db93813798ea956ff
Leonard <leonardbinet@gmail.com>
2017-04-02 21:55:04 +0200

    Functional celery beat

--- etl_tasks/celery_app.py
+++ etl_tasks/celery_app.py
@@ -4,6 +4,7 @@
 from os import sys, path
 
 from celery import Celery
+from celery.schedules import crontab
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
@@ -20,3 +21,17 @@ app = Celery('etl_tasks',
 app.conf.update(
     result_expires=3600,
 )
+
+
+app.conf.beat_schedule = {
+    # Executes every two minutes
+    'extract_api_two_minutes': {
+        'task': 'tasks.extract_api_once_all_stations',
+        'schedule': crontab(minute='*/2'),
+    },
+    # Executes every Monday morning at 7:30 a.m.
+    'extract_schedule_weekly': {
+        'task': 'tasks.extract_schedule',
+        'schedule': crontab(hour=7, minute=30, day_of_week=1),
+    },
+}
